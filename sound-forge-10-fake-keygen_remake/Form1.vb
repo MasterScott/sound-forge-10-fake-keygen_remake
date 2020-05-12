25bdfb65b178539aaeb65ff03e36c7aa704c659c
miso-xyz <61099345+miso-xyz@users.noreply.github.com>
2020-05-12 03:41:11 +0100

    Add files via upload

---
+++ sound-forge-10-fake-keygen_remake/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Dim blnButtonDown, blnButtonDown_
+    Private Sub Button1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Button1.Paint
+        If blnButtonDown = False Then
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Raised)
+        Else
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Sunken)
+        End If
+    End Sub
+    Private Sub Button1_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseDown
+        blnButtonDown = True
+        sender.Invalidate()
+    End Sub
+
+    Private Sub Button1_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseUp
+        blnButtonDown = False
+        sender.Invalidate()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+    Private Sub Button2_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Button2.Paint
+        If blnButtonDown_ = False Then
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Raised)
+        Else
+            ControlPaint.DrawBorder3D(e.Graphics, sender.ClientRectangle,
+                    Border3DStyle.Sunken)
+        End If
+    End Sub
+    Private Sub Button2_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button2.MouseDown
+        blnButtonDown_ = True
+        sender.Invalidate()
+    End Sub
+
+    Private Sub Button2_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button2.MouseUp
+        blnButtonDown_ = False
+        sender.Invalidate()
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim keys As New List(Of String)({"HQ-DHQEX1-69D9H1-MDRB8P", "HQ-CT63TP-LP3271-QNR5ZX", "HQ-ECBXT7-77CFTB-BR4GYV", "HQ-YHXT9R-BBF4BF-VG9YVY", "HQ-EDV67S-QU2ZF1-BR4GYV", "HQ-3TFGYX-IIS5TY-9RNM52", "HQ-4P3DFT-3DFFTB-B24G6T"})
+        Dim rng_ As New Random
+        RichTextBox1.Text = keys(rng_.Next(0, 6))
+    End Sub
+End Class
